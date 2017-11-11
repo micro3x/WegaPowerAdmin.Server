9c2eb328c90d37b61324b2d83090990ab268d02e
micro3x <micro3x@gmail.com>
2017-11-11 15:18:56 +0200

    Roles Service and shared service extend

--- src/app/_services/shared.service.js
+++ src/app/_services/shared.service.js
@@ -5,24 +5,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 var BehaviorSubject_1 = require("rxjs/BehaviorSubject");
+var rest_service_1 = require("./rest.service");
 var SharedService = (function () {
-    function SharedService() {
-        // private loggedUser = new Subject<User>();
+    function SharedService(restService) {
+        this.restService = restService;
         this.loggedUser = new BehaviorSubject_1.BehaviorSubject(null);
         this.currentUser = this.loggedUser.asObservable();
     }
     SharedService.prototype.userLogin = function (user) {
         this.loggedUser.next(user);
+        this.populateRoles();
     };
     SharedService.prototype.userLogout = function () {
         this.loggedUser.next(null);
     };
+    SharedService.prototype.populateRoles = function () {
+        this.restService.get('/classes/_Role').map(function (data) {
+            console.log(data);
+        });
+    };
     return SharedService;
 }());
 SharedService = __decorate([
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [rest_service_1.RestService])
 ], SharedService);
 exports.SharedService = SharedService;
 //# sourceMappingURL=shared.service.js.map
--- src/app/_services/shared.service.ts
+++ src/app/_services/shared.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject }    from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { User } from '../models/index';
+import { User, Role } from '../models/index';
+import { RestService } from './rest.service'
 
 @Injectable()
 export class SharedService {
-    
-    // private loggedUser = new Subject<User>();
-    private loggedUser = new BehaviorSubject<User>(null);
 
+    private loggedUser = new BehaviorSubject<User>(null);
     currentUser = this.loggedUser.asObservable();
-    
-    userLogin(user: User){
-        this.loggedUser.next(user);
+
+    roles: Role[];
+
+    constructor(private restService: RestService) {
+
     }
 
-    userLogout(){
+    userLogin(user: User) {
+        this.loggedUser.next(user);
+        this.populateRoles();
+    }
+
+    userLogout() {
         this.loggedUser.next(null);
+    }
+
+    private populateRoles(){
+        this.restService.get('/classes/_Role').map(
+            data => {
+                console.log(data);
+            }
+        )
     }
 
 }
